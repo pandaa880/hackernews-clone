e4fa66860e0f71648617ac10b00cbe8a7f2870cd
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-09-25 09:52:36 +0530

    initial setup
      configure tailwindcss & add packages for apollo client

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    colors: {
+      gray: "#828282",
+      organge: "#ff6600",
+      backgroundGray: "rgb(246,246,239)",
+    },
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+  },
+  variants: {},
+  plugins: [],
+};
